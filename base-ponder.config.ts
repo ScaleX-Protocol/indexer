183ebb06daf246f6ca08f78e86a21180e53e0abc
Renaka <renaka@localhost-3.local>
2025-08-25 07:22:28 +0700

    feat: enable faucet indexing from different chain

--- base-ponder.config.ts
+++ base-ponder.config.ts
@@ -45,12 +45,10 @@ const contracts: any = {
 	},
     Faucet: {
         abi: FaucetABI || [],
-        network: "network",
-        address: getAddress(
-            (process.env.FAUCET_CONTRACT_ADDRESS as `0x${string}`) ||
-            default_address
-        ),
-        startBlock: Number(process.env.START_BLOCK) || undefined,
+        network: "faucet-network",
+        address: getAddress((process.env.FAUCET_CONTRACT_ADDRESS as `0x${string}`) || default_address),
+        startBlock: Number(process.env.FAUCET_START_BLOCK) || Number(process.env.START_BLOCK) || undefined,
+		endBlock: process.env.FAUCET_END_BLOCK || process.env.END_BLOCK as number | undefined,
     },
 };
 
@@ -62,6 +60,12 @@ export function getBaseConfig() {
 				transport: http(process.env.PONDER_RPC_URL),
 				pollingInterval: Number(process.env.POLLING_INTERVAL) || 100,
 				maxRequestsPerSecond: Number(process.env.MAX_REQUESTS_PER_SECOND) || 250,
+			},
+			"faucet-network": {
+				chainId: Number(process.env.FAUCET_CHAIN_ID) || Number(process.env.CHAIN_ID) || 4661,
+				transport: http(process.env.FAUCET_RPC_URL || process.env.PONDER_RPC_URL || '/api/rpc/appchain-testnet'),
+				pollingInterval: Number(process.env.FAUCET_POLLING_INTERVAL) || Number(process.env.POLLING_INTERVAL) || 100,
+				maxRequestsPerSecond: Number(process.env.FAUCET_MAX_REQUESTS_PER_SECOND) || Number(process.env.MAX_REQUESTS_PER_SECOND) || 250,
 			},
 		},
 		contracts: contracts,
